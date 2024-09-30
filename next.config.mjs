2334a31d6155159426e9f3d64eb4e7392545a350
tinon1004 <tinon1004@ajou.ac.kr>
2024-09-30 16:51:08 +0900

    Fix: change import next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-const withPWA = require('next-pwa')({
-    dest: 'public', // PWA 관련 파일이 저장될 폴더 (예: service worker)
-  });
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    // 기존 Next.js 설정을 여기에 추가할 수 있습니다
-  };
-  
-  module.exports = withPWA(nextConfig);
+import withPWA from 'next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  dest: 'public', // PWA 관련 파일을 저장할 폴더
+});
+
+export default nextConfig;
+
   
