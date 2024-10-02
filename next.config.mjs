464aa434fc93e9fae539b6a2a051a182ba2952b3
tinon1004 <tinon1004@ajou.ac.kr>
2024-10-03 07:10:46 +0900

    Refactor: remove comment code

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,17 @@
-// import withPWA from 'next-pwa';
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-// };
-
-// // PWA 설정을 적용
-// export default withPWA({
-//   ...nextConfig,
-//   dest: 'public', // PWA 관련 파일을 저장할 폴더
-// });
-
-// next.config.mjs
-
 import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,      // Enable React strict mode for improved error handling
-    swcMinify: true,            // Enable SWC minification for improved performance
+    reactStrictMode: true,      
+    swcMinify: true,            
     compiler: {
-        removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
+        removeConsole: process.env.NODE_ENV !== "development"    
     }
 };
 
 export default withPWA({
-    dest: "public",         // destination directory for the PWA files
-    disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
-    register: true,         // register the PWA service worker
-    skipWaiting: true,      // skip waiting for service worker activation
+    dest: "public",        
+    disable: process.env.NODE_ENV === "development",        
+    register: true,        
+    skipWaiting: true,      
 })(nextConfig);
