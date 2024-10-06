93bdec43354011ec57da88cd1e914d0f70ebb252
tinon1004 <tinon1004@ajou.ac.kr>
2024-10-07 01:32:41 +0900

    Refactor: changing emotion naming

--- src/app/EmotionData.ts
+++ src/app/EmotionData.ts
@@ -1,24 +1,40 @@
-export type Emotion = '분노' | '경멸' | '역겨움' | '두려움' | '행복' | '중립' | '슬픔' | '놀람';
+export type Emotion = 
+  | '행복한, 좋은 표정' 
+  | '짜증난, 싫은 표정' 
+  | '두려운, 무서운 표정' 
+  | '화난, 분노의 표정' 
+  | '슬픈, 우울한 표정' 
+  | '놀란, 놀라는 표정' 
+  | '덤덤한, 무표정' 
 
 export interface ImageData {
   src: string;
   correctEmotion: Emotion;
 }
 
-export const emotions: Emotion[] = ['분노', '경멸', '역겨움', '두려움', '행복', '중립', '슬픔', '놀람'];
+export const emotions: Emotion[] = [
+  '행복한, 좋은 표정', 
+  '짜증난, 싫은 표정', 
+  '두려운, 무서운 표정', 
+  '화난, 분노의 표정', 
+  '슬픈, 우울한 표정', 
+  '놀란, 놀라는 표정', 
+  '덤덤한, 무표정'
+];
+
 
 export const imageData: ImageData[] = [
   {
     src: '/img/emotion/angry.png',
-    correctEmotion: '분노'
+    correctEmotion: '화난, 분노의 표정'
   },
   {
     src: '/img/emotion/happy.png',
-    correctEmotion: '행복'
+    correctEmotion: '행복한, 좋은 표정'
   },
   {
     src: '/img/emotion/sad.png',
-    correctEmotion: '슬픔'
+    correctEmotion: '슬픈, 우울한 표정'
   }
 ];
 
@@ -26,14 +42,3 @@ export function getRandomImage(): ImageData {
   const randomIndex = Math.floor(Math.random() * imageData.length);
   return imageData[randomIndex];
 }
-
-export const emotionColors = {
-  '분노': 'bg-red-400 hover:bg-red-500',
-  '경멸': 'bg-purple-400 hover:bg-purple-500',
-  '역겨움': 'bg-green-400 hover:bg-green-500',
-  '두려움': 'bg-blue-400 hover:bg-blue-500',
-  '행복': 'bg-yellow-400 hover:bg-yellow-500',
-  '중립': 'bg-gray-400 hover:bg-gray-500',
-  '슬픔': 'bg-indigo-400 hover:bg-indigo-500',
-  '놀람': 'bg-pink-400 hover:bg-pink-500'
-} as const;
