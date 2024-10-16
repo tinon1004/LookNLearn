e942c885d235e5359950bc5edc6755a980336611
tinon1004 <tinon1004@ajou.ac.kr>
2024-10-17 00:17:25 +0900

    Feat: add login field at LandingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function LandingPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
       <div className="max-w-md w-full space-y-8 text-center">
-        {/* 로고 */}
         <div className="mx-auto h-40 w-80 relative">
           <Image
             src="/img/Logo.png" 
@@ -30,14 +29,26 @@ export default function LandingPage() {
           나와의 눈맞춤으로 표정을 학습해요!
         </p>
         
-        <div className="space-y-4">
+        <div className="space-y-2">
+          <input
+            type="text"
+            placeholder="아이디를 입력해 주세요"
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해 주세요"
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+        </div>
+
+        
           <button
             onClick={MovePage}
             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
           >
             시작하기
           </button>
-        </div>
         
       </div>
     </div>
