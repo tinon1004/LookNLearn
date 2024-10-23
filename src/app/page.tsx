ba6bab82694d2fe79df166f996ecb38ffb938723
tinon1004 <tinon1004@ajou.ac.kr>
2024-10-24 05:15:55 +0900

    Refactor: add signup button and edit design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,10 @@ export default function LandingPage() {
   const MovePage = () => {
     router.push('/main');
   };
+
+  // const MoveToSignup = () => {
+  //   router.push('/signup');
+  // };
  
  
   return (
@@ -29,27 +33,34 @@ export default function LandingPage() {
           나와의 눈맞춤으로 표정을 학습해요!
         </p>
         
-        <div className="space-y-2">
+        <div className="flex flex-col items-center space-y-4">
           <input
             type="text"
             placeholder="아이디를 입력해 주세요"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-60 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
           />
           <input
             type="password"
             placeholder="비밀번호를 입력해 주세요"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-60 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
           />
-        </div>
 
-        
+          <button
+            // onClick={MoveToSignup}
+            className="text-gray-600 hover:text-gray-800 text-sm font-medium"
+          >
+            회원가입 하기
+          </button>
+
           <button
             onClick={MovePage}
-            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+            className="w-60 px-3 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
           >
-            시작하기
+            로그인
           </button>
-        
+
+        </div>
+
       </div>
     </div>
   );
