f98b2fdec4a4ee587c66a70796e7c42289b153fe
tinon1004 <tinon1004@ajou.ac.kr>
2024-11-12 14:45:31 +0900

    Feat: add Logo and dropdown

--- src/app/main/MainPage.tsx
+++ src/app/main/MainPage.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Calendar from './Calendar';
 import StickerStatus from './StickerStatus';
 import BadgeStatus from './BadgeStatus';
+import Image from 'next/image';
 
 const MainPage: React.FC = () => {
   const router = useRouter();
-  
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const handleNavigation = (path: string): void => {
     router.push(path);
   };
@@ -21,34 +23,77 @@ const MainPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-start h-screen p-4">
-      <div className="absolute top-4 left-4">
-        <button
-          onClick={() => router.push('/')}
-          className="bg-gray-500 text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition duration-300"
-        >
-
-        </button>
-      </div>
-      <div className="flex justify-center mb-4">
-        <button
-          onClick={() => router.push('/main/introduction')} 
-          className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
-        >
-          서비스 소개
-        </button>
-        <button 
-          onClick={() => router.push('/main/step1')} 
-          className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 transition duration-300 ml-4"
-        >
-          학습하기
-        </button>
-      </div>
-      <Calendar onDayClick={handleDayClick} />
-      <div className="flex gap-4 w-full max-w-5xl mt-4">
-        <StickerStatus />
-        <BadgeStatus />
-      </div>
+    <div className="flex flex-col min-h-screen items-center">
+      <header className="w-full px-4 py-2">
+        <div className="w-[640px] mx-auto flex justify-between items-center mt-3">
+          <div className="h-10 w-20 relative">
+            <Image
+              src="/img/Left_Logo.png"
+              alt="Logo"
+              fill
+              className="object-contain"
+            />
+          </div>
+         
+          <div className="relative">
+            <div 
+              className="w-8 h-8 relative cursor-pointer"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              <Image
+                src="/icons/list.png"
+                alt="list"
+                fill
+                className="object-contain"
+              />
+            </div>
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 w-40 bg-white rounded-md shadow-lg z-10">
+                <button
+                  onClick={() => {
+                    handleNavigation('/report');
+                    setIsDropdownOpen(false);
+                  }}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                >
+                  보고서 보기
+                </button>
+                <button
+                  onClick={() => {
+                    handleNavigation('/logout');
+                    setIsDropdownOpen(false);
+                  }}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                >
+                  로그아웃 하기
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </header>
+     
+        <div className="max-w-5xl mx-auto mt-3">
+        <Calendar onDayClick={handleDayClick} />
+        <div className="flex gap-4 w-full max-w-5xl mt-4">
+          <StickerStatus />
+          <BadgeStatus />
+        </div>
+        <div className="flex justify-center mt-8 mb-4">
+          <button
+            onClick={() => router.push('/main/introduction')} 
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+          >
+            서비스 소개
+          </button>
+          <button 
+            onClick={() => router.push('/main/step1')} 
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 transition duration-300 ml-4"
+          >
+            학습하기
+          </button>
+        </div>
+        </div>
     </div>
   );
 }
