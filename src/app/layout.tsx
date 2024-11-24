df74aec2199eb435ecd2e1bd135581916c6cb418
tinon1004 <tinon1004@ajou.ac.kr>
2024-11-24 15:52:33 +0900

    Fix: remove unused import statement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { useEffect } from 'react';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./context/AuthProvider";
